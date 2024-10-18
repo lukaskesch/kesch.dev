51ab72f676cecaed58a973607d1279ef2ce64153
Lukas Kesch <lukas.kesch@gmail.com>
2024-10-18 20:06:28 +0200

    Add very basic header with nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,32 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header className="flex flex-row justify-between items-center mx-4 my-2">
+          <nav className="flex flex-row gap-4 items-center">
+            <a href="/">
+              <div className="text-xl font-bold">Kesch.dev</div>
+            </a>
+            <ul className="flex flex-row gap-4">
+              <li>
+                <a href="/projects">Projects</a>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <ul className="flex flex-row items-center gap-4">
+              <li>
+                <a href="https://focusjournal.io" target="_blank">
+                  Focus Journal
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/lukaskesch" target="_blank">
+                  GitHub
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Projects() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8">
-      <h1>Home</h1>
+      <h1>Projects</h1>
     </div>
   );
 }
