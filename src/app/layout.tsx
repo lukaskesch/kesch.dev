d6dd585fe880f64a5f89b7ae23f081985736d883
Lukas Kesch <lukas.kesch@gmail.com>
2024-10-18 20:32:43 +0200

    Add fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <header className="flex flex-row justify-between items-center mx-4 my-2">
