b817b6de652f620ced4f10eab673abb5ea3d4f09
Lukas Kesch <lukas.kesch@gmail.com>
2024-10-20 14:23:52 +0200

    Add docker start script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,20 +24,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <header className="flex flex-row justify-between items-center mx-4 my-2">
           <nav className="flex flex-row gap-4 items-center">
             <a href="/">
-              <div className="text-xl font-bold">Kesch.dev</div>
+              <div className="text-2xl font-bold">Kesch.dev</div>
             </a>
             <ul className="flex flex-row gap-4">
               <li>
